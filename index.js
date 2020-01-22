64c6917b5e6f063a6bd7bd54fb330617fac7f9ee
Yeongil Yu <polosesy@nate.com>
2020-01-22 15:38:33 +0900

    Express Core:Middlewares

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const express = require('express')
+import express from "express";
+import logger from "morgan";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express()
 const PORT = 4000
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome (req, res) {
-    res.send("Hello from home!");
-}
+const handleHome = (req, res) => res.send("Hello from home!");
 
-function handleProfile (req, res){
-    res.send("You are on my profile");
-}
+const handleProfile = (req, res)=> res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
