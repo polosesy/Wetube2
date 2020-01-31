eab4da59644dac335c6060b3f2c6f31cf9b01399
Yeongil Yu <polosesy@nate.com>
2020-01-31 19:01:52 +0900

    Change Profile HTML

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import routes from"../routes";
+import { videos, upload, videoDetail, deleteVideo, editVideo } from "../controller/videoController";
+
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos,(req, res)=> res.send("Video"));
-videoRouter.get(routes.upload,(req, res)=> res.send("Upload"));
-videoRouter.get(routes.videosDetail,(req, res)=> res.send("Video Detail"));
-videoRouter.get(routes.editVideo,(req, res)=> res.send("Edit video"));
-videoRouter.get(routes.deleteVideo,(req, res)=> res.send("Delete Video"));
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videosDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
