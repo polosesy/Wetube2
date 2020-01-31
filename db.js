9c551731cd6f05d311a4b0019e17de6095802d89
Yeongil Yu <polosesy@nate.com>
2020-01-31 20:00:29 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,39 @@
+export const videos = [
+    {
+        id:324393,
+        title:"Video Awesome",
+        description:"This is something I love",
+        views: 24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Yeongil",
+            email: "polosesy@nate.com"
+        }
+    },
+    {
+        id:121212,
+        title:"Video Super",
+        description:"This is something I love",
+        views: 24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Yeongil",
+            email: "polosesy@nate.com"
+        }
+    },
+    {
+        id:3242222,
+        title:"Video nice",
+        description:"This is something I love",
+        views: 24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Yeongil",
+            email: "polosesy@nate.com"
+        }
+    }
+];
+
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from"../routes";
-import { videos, upload, videoDetail, deleteVideo, editVideo } from "../controller/videoController";
+import { //videos, 
+    upload, videoDetail, deleteVideo, editVideo } from "../controller/videoController";
 
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
+//videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videosDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
