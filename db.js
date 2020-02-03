6019274df1ac527b6c46049bf0ee6df5cc229f10
Yeongil Yu <polosesy@nate.com>
2020-02-03 10:49:29 +0900

    Home Controller part Two

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title:"Video Awesome",
         description:"This is something I love",
         views: 24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/details/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Yeongil",
@@ -16,7 +16,7 @@ export const videos = [
         title:"Video Super",
         description:"This is something I love",
         views: 24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/details/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Yeongil",
@@ -28,7 +28,7 @@ export const videos = [
         title:"Video nice",
         description:"This is something I love",
         views: 24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/details/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Yeongil",
