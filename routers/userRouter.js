38394dfb8bc4cc546d5bb633c5bd4ef5259f4622
Yeongil Yu <polosesy@nate.com>
2020-03-17 14:32:31 +0900

    3.11 Installing ESLint

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,14 +1,15 @@
 import express from "express";
 import routes from "../routes";
-import { users,editProfile,changePassword,usersDetail } from "../controller/userController";
-
+import {
+  editProfile,
+  changePassword,
+  usersDetail
+} from "../controller/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail(), usersDetail);
-
 
 export default userRouter;
