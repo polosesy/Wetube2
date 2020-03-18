4301a14a3605b0da21dc1ceebb45c2ab08e25c8e
Yeongil Yu <polosesy@nate.com>
2020-03-19 03:52:15 +0900

    4.3 ES6 with Webpack

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
-
 const app = express();
 
 app.use(helmet());
-app.set('view engine',"pug");
+app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
-app.use(routes.videos,videoRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
