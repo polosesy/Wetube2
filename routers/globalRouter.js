9c725f9ac78c5525294891a41849bb63b1da97cc
Yeongil Yu <polosesy@nate.com>
2020-03-29 15:34:55 +0900

    6.3 Loggin the User In

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,27 +1,25 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controller/videoController";
-import { 
-    getJoin, 
-    getLogin, 
-    postLogin,
-    logout,
-    postJoin 
+import {
+  getJoin,
+  getLogin,
+  postLogin,
+  logout,
+  postJoin
 } from "../controller/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
+globalRouter.post(routes.join, postJoin, postLogin);
 
-globalRouter.get(routes.login,getLogin);
-globalRouter.post(routes.login,postLogin);
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
 
-globalRouter.get(routes.home,home);
-globalRouter.get(routes.search,search);
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
 
-
-globalRouter.get(routes.logout,logout);
-
+globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
