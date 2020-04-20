a92531fe21e1c939a712ae15e76d43e8f4685714
Yeongil Yu <polosesy@nate.com>
2020-04-20 13:47:32 +0900

    6.5 MongoStore and Middlewares

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,11 +5,12 @@ import {
   changePassword,
   usersDetail
 } from "../controller/userController";
+import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), usersDetail);
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,30 +1,30 @@
 import express from "express";
-import routes from"../routes";
-import { //videos, 
-    getUpload,
-    postUpload, 
-    videoDetail, 
-    deleteVideo,
-    getEditVideo,
-    postEditVideo
+import routes from "../routes";
+import {
+  //videos,
+  getUpload,
+  postUpload,
+  videoDetail,
+  deleteVideo,
+  getEditVideo,
+  postEditVideo
 } from "../controller/videoController";
-import { uploadVideo } from "../middlewares";
-
+import { uploadVideo, onlyPrivate } from "../middlewares";
 
 const videoRouter = express.Router();
 
 // Upload
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 
 // Vidoe Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 // Edit Video
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
 
 // Delete Video
-videoRouter.get(routes.deleteVideo(), deleteVideo);
+videoRouter.get(routes.deleteVideo(), onlyPrivate, deleteVideo);
 
 export default videoRouter;
