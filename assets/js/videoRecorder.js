1146c04b00d65675662a0659f11da6fb5315d30d
Yeongil Yu <polosesy@nate.com>
2020-06-02 18:28:30 +0900

    9.2 Recording video part Two

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,26 +3,40 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = (event) => {
-  console.log(event);
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
   console.log(videoRecorder);
   videoRecorder.addEventListener("dataavilable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
 };
 
 const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
-      //video: { width: 1280, height: 720 }, 비디오 레코딩
+      // video: { width: 1280, height: 720 }, 비디오 레코딩
     });
     videoPreview.srcObject = stream;
-    videoPreview.muted = true; //비디오 레코딩시 음소거
+    videoPreview.muted = true; // 비디오 레코딩시 음소거
     videoPreview.play();
     recordBtn.innerHTML = "Stop recording";
     streamObject = stream;
