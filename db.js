161c557148b5d928ed09939c6ce009703305dc23
yeongil.yu <polosesy@nate.com>
2020-08-25 16:47:21 +0900

    passport.js bug fix & .env(must needgit add .)

--- db.js
+++ db.js
@@ -1,19 +1,21 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 mongoose.connect(
-   process.env.MONGO_URL,
-   {
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
- }
+    useFindAndModify: false,
+  }
 );
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅  Connected to DB");
-const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);
