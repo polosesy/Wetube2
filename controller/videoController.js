1c7eed48a7218b3777b1e146994206dfbd3da916
yeongil.yu <polosesy@nate.com>
2020-09-07 20:16:36 +0900

    11.2 Blob Duration Bug

--- controller/videoController.js
+++ controller/videoController.js
@@ -35,7 +35,6 @@ export const postUpload = async (req, res) => {
     body: { title, description },
     file: { location },
   } = req;
-  console.log(req.file);
   const newVideo = await Video.create({
     fileUrl: location,
     title,
